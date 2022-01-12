aa5d5873af532026159e6d09b55c4b23359df655
Graham Johnson <grahamkeithjohnson@gmail.com>
2022-01-12 12:03:18 -0700

    Version 1.0.0 initial commit

---
+++ CharacterBitmap.h
@@ -0,0 +1,14 @@
+/*
+ * CharacterBitmap.h
+ *
+ *  Created on: Dec 29, 2021
+ *      Author: Graham Johnson
+ */
+
+#ifndef CHARACTER_BITMAP_H_
+#define CHARACTER_BITMAP_H_
+#endif /* CHARACTER_BITMAP_H_ */
+
+const extern uint64_t CHARACTER_BITMAPS_8x8[128];
+
+uint64_t getCharacterBitmap(char c);
